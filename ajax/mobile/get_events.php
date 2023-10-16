8ddad2f002306b23b148be3ed3191f77ddbf1812
Jeffred Lim <iamjeffredlim@gmail.com>
2023-10-16 15:02:40 +0800

    added participants

--- ajax/mobile/get_events.php
+++ ajax/mobile/get_events.php
@@ -9,7 +9,7 @@ include '../../core/config.php';
 $data = json_decode(file_get_contents("php://input"));
 $user_id = $data->user_id;
 
-$sql = "SELECT * FROM tbl_event_judges ej LEFT JOIN tbl_events e ON ej.event_id=e.event_id WHERE judge_id='$user_id' ORDER BY e.event_start DESC";
+$sql = "SELECT e.event_id, ej.event_judge_id, e.event_name, e.event_description, e.event_start, e.event_status FROM tbl_event_judges ej LEFT JOIN tbl_events e ON ej.event_id=e.event_id WHERE judge_id='$user_id' ORDER BY e.event_start DESC";
 $fetch = $mysqli->query($sql);
 
 $response = array();
@@ -17,11 +17,11 @@ $response = array();
 while ($row = $fetch->fetch_assoc()) {
     $list = array();
     $list['event_judge_id'] = $row['event_judge_id'];
+    $list['event_id'] = $row['event_id'];
     $list['event_name'] = $row['event_name'];
     $list['event_description'] = $row['event_description'];
     $list['event_start'] = date("M d, Y", strtotime(($row['event_start'])));
     $list['status'] = $row['event_status'];
-    //S=Open For Registration;P=Ongoing;F=Finish
     array_push($response, $list);
 }
 
--- ajax/mobile/get_participants.php
+++ ajax/mobile/get_participants.php
@@ -9,13 +9,27 @@ include '../../core/config.php';
 $data = json_decode(file_get_contents("php://input"));
 $event_id = $data->event_id;
 
-$sql = "SELECT * FROM tbl_event_participants WHERE event_id='$event_id'";
+$response_criteria = array();
+$fetch_criteria = $mysqli->query("SELECT * from tbl_event_criterias WHERE event_id='$event_id'");
+while ($criteria_row = $fetch_criteria->fetch_assoc()) {
+    $list_criteria = array();
+    $list_criteria['criteria_id'] = $criteria_row['criteria_id'];
+    $list_criteria['criteria'] = $criteria_row['criteria'];
+    $list_criteria['points'] = $criteria_row['points'];
+    array_push($response_criteria, $list_criteria);
+}
+
+$sql = "SELECT * FROM tbl_participants p LEFT JOIN tbl_event_participants ep ON p.participant_id=ep.participant_id WHERE event_id='$event_id'";
 $fetch = $mysqli->query($sql);
 
 $response = array();
-
 while ($row = $fetch->fetch_assoc()) {
-    array_push($response, $row);
+    $list = array();
+    $list['participant_id'] = $row['participant_id'];
+    $list['participant_name'] = $row['participant_name'];
+    $list['participant_affiliation'] = $row['participant_affiliation'];
+    $list['criteria'] = $response_criteria;
+    array_push($response, $list);
 }
 
 echo json_encode($response);
