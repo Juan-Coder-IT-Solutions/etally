be18a582b5af1183864d069b0805442b76a991d9
Jeffred Lim <iamjeffredlim@gmail.com>
2023-10-18 14:24:12 +0800

    Update get_events.php

--- ajax/mobile/get_events.php
+++ ajax/mobile/get_events.php
@@ -9,7 +9,7 @@ include '../../core/config.php';
 $data = json_decode(file_get_contents("php://input"));
 $user_id = $data->user_id;
 
-$sql = "SELECT e.event_id, ej.event_judge_id, e.event_name, e.event_description, e.event_start, e.event_status FROM tbl_event_judges ej LEFT JOIN tbl_events e ON ej.event_id=e.event_id WHERE judge_id='$user_id' ORDER BY e.event_start DESC";
+$sql = "SELECT e.event_id, ej.event_judge_id, e.event_name, e.event_description, e.event_start, e.event_status, e.event_mechanics FROM tbl_event_judges ej LEFT JOIN tbl_events e ON ej.event_id=e.event_id WHERE judge_id='$user_id' ORDER BY e.event_start DESC";
 $fetch = $mysqli->query($sql);
 
 $response = array();
@@ -19,6 +19,7 @@ while ($row = $fetch->fetch_assoc()) {
     $list['event_judge_id'] = $row['event_judge_id'];
     $list['event_id'] = $row['event_id'];
     $list['event_name'] = $row['event_name'];
+    $list['event_mechanics'] = $row['event_mechanics'];
     $list['event_description'] = $row['event_description'];
     $list['event_start'] = date("M d, Y", strtotime(($row['event_start'])));
     $list['status'] = $row['event_status'];
