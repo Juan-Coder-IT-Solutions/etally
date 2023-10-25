2187038ed9f78e680d45612c4a994d163cc0483f
Jeffred Lim <iamjeffredlim@gmail.com>
2023-10-25 13:48:50 +0800

    Update config.php

--- core/config.php
+++ core/config.php
@@ -3,8 +3,8 @@
 // START THE SESSION
 session_start();
 
-$mysqli = new mysqli("localhost", "root", "", "etally_db");
-// $mysqli = new mysqli("localhost", "u814036432_root", "#VM>:m&8oQ", "u814036432_etally");
+//$mysqli = new mysqli("localhost", "root", "", "etally_db");
+$mysqli = new mysqli("localhost", "u814036432_root", "#VM>:m&8oQ", "u814036432_etally");
 // Check connection
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: " . $mysqli->connect_error;
