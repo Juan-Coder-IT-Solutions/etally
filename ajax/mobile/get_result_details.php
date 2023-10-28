419b8072cfec71f1ff0cc94ddaa6d3f340d6d3fc
Jeffred Lim <iamjeffredlim@gmail.com>
2023-10-28 14:51:50 +0800

    Update get_result_details.php

--- ajax/mobile/get_result_details.php
+++ ajax/mobile/get_result_details.php
@@ -17,11 +17,14 @@ if (isset($event_id) && isset($judge_id)) {
     $response['data'] = array();
 
     // fetch criteria
-    $criteria = array();
+    $criteria = [];
     $fetch_criteria = $mysqli->query("SELECT ch_id, criteria, points FROM tbl_event_criteria_header WHERE event_id='$event_id' ORDER BY ch_id ASC");
     while ($criteria_row = $fetch_criteria->fetch_assoc()) {
-        $criteria = $criteria_row;
-        array_push($response['criteria'], $criteria);
+        $fetch_count = $mysqli->query("SELECT count(criteria_id) FROM tbl_event_criterias WHERE ch_id='$criteria_row[ch_id]'");
+        $count_row = $fetch_count->fetch_array();
+        $criteria_row['criteria_details_count'] = $count_row[0];
+        $criteria[] = $criteria_row;
+        array_push($response['criteria'], $criteria_row);
     }
 
 
@@ -38,21 +41,34 @@ if (isset($event_id) && isset($judge_id)) {
             // $participants_row['participant_overall_rank'] = $participants_row['rank'];
             $fetch_ranks = $mysqli->query("SELECT * from tbl_event_ranks WHERE event_id='$event_id' and participant_id='$participants_row[participant_id]' and judge_id='$judges_row[judge_id]'");
             $ranks_row = $fetch_ranks->fetch_assoc();
-            $participants_row['scores'] = $ranks_row['scores'];
+            $participants_row['overall_total'] = $ranks_row['scores'];
             $participants_row['judge_rank'] = $ranks_row['rank'];
             $total_rank += $ranks_row['rank'];
 
 
             // fetch scores per criteria
             $response_scores = array();
-            foreach ($criteria as $val) {
-                $fetch_scores = $mysqli->query("SELECT criteria_id, criteria, points FROM tbl_event_criterias WHERE event_id='$event_id'");
-                while ($scores_row = $fetch_scores->fetch_assoc()) {
-                    $response_scores = $scores_row;
-                    array_push($response_scores, $scores_row);
+            $total_crit_key = 10000;
+            foreach ($criteria as $key => $value) {
+                $fetch_criteria_details = $mysqli->query("SELECT criteria_id, criteria, points, ch_id FROM tbl_event_criterias WHERE ch_id='$value[ch_id]'");
+                $total = 0;
+                while ($criteria_details_row = $fetch_criteria_details->fetch_assoc()) {
+                    $fetch_scores = $mysqli->query("SELECT points from tbl_event_scores WHERE criteria_id='$criteria_details_row[criteria_id]' and event_id='$event_id' and participant_id='$participants_row[participant_id]' and judge_id='$judges_row[judge_id]' ");
+                    $points = $fetch_scores->fetch_array();
+                    $criteria_details_row['score'] = $points[0] * 1;
+                    $total += $points[0] * 1;
+
+                    array_push($response_scores, $criteria_details_row);
                 }
-                $participants_row['score_details'] = $response_scores;
+
+                $total_score['ch_id'] = $total_crit_key++;
+                $total_score['criteria_id'] = $total_crit_key++;
+                $total_score['criteria'] = "Total";
+                $total_score['score'] = $total;
+                array_push($response_scores, $total_score);
             }
+
+            $participants_row['score_details'] = $response_scores;
 
 
             array_push($response_participants, $participants_row);
